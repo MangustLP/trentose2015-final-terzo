8ebd889330405ceec89126213f82659bfc5436e5
MangustLP <olly.pilat@gmail.com>
2016-02-10 15:47:17 +0100

    ciao

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
+var scoref = 0;
+var phrase1= "";
+
+$(document).ready(function(){
+
+    $(".btn").on("click", function(event){
+       
+        var temps = document.getElementById("inp").value;
+        ParlaModel.detectLanguage(temps);
+        
+        
+                    
+    });
+    
+});
+
 var ParlaModel = {
   
    /* Langs contain the list of language with their corresponding
     * dictionaries 
     */
-   langs : languages, 
+   //langs : languages, 
   
    /* Returns the score of the given phrase in the given language
     * The score is computed simply as the number of words - in the
@@ -12,8 +28,57 @@ var ParlaModel = {
     *         null, if language is not valid
     */
    getLanguageScore : function (phrase, language){
-     // write your code here, but don't add any additional 
-     // parameters to the the function
+      
+
+       var array_parole = phrase1.split(" ");
+       alert(array_parole.length);
+       var score = 0;
+       var i = 0;
+       var j = 0;
+
+      if (language == languages[0].name){ 
+           
+           for (i = 0; i < ita.length;i ++){
+              
+               
+               for (j = 0; j < array_parole.length; j++){
+                   
+                   if (array_parole[j] == ita[i]) {scoref = scoref+1;}
+                   
+                   
+               }
+           }
+           
+           
+       }
+       else if (language == languages[1].name){ 
+           
+           for (i = 0; i < spa.length;i ++){
+              
+               
+               for (j = 0; j < array_parole.length; j++){
+                   
+                   if (array_parole[j] == spa[i]) {scoref = scoref+1;}
+                   
+                   
+               }
+           }
+           
+        
+       else if (language == languages[2].name){ 
+           
+           for (i = 0; i < eng.length;i ++){
+              
+               
+               for (j = 0; j < array_parole.length; j++){
+                   
+                   if (array_parole[j] == eng[i]) {scoref = scoref+1;}
+                   
+                   
+               }
+           }
+        
+        
    },
   
    /* Returns the name of the language in which the phrase
@@ -21,8 +86,11 @@ var ParlaModel = {
     * returns the language with higher *score*. 
     */
    detectLanguage : function (phrase) {
-     // write your code here, but don't add any additional 
-     // parameters to the the function     
+       phrase1 = phrase;
+       ParlaModel.getLanguageScore(phrase1,"English");
+       alert(scoref);
+       
+       
    }
          
 };
